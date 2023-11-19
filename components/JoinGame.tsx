7f403fe26f4eb46b26f9c7b9e764ee612fc11dc7
keremcs <147047280+keremcs@users.noreply.github.com>
2023-11-19 21:03:21 +0300

    game loading

--- components/JoinGame.tsx
+++ components/JoinGame.tsx
@@ -5,9 +5,11 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
 
 export default function JoinGame() {
   const [game, setGame] = useState("");
+  const [pending, setPending] = useState(false);
   const router = useRouter();
   return (
     <div className="flex flex-col justify-center items-center gap-8">
@@ -24,15 +26,23 @@ export default function JoinGame() {
           onKeyDown={(e) => {
             if (e.key === "Enter" && Number(game) > 0) {
               e.preventDefault();
+              setPending(true);
               router.push(`/game/${game}`);
             }
           }}
         />
         <SignedIn>
           {Number(game) > 0 ? (
-            <Button asChild>
-              <a href={`/game/${game}`}>Enter</a>
-            </Button>
+            pending ? (
+              <Button disabled>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Loading
+              </Button>
+            ) : (
+              <Button asChild onClick={() => setPending(true)}>
+                <a href={`/game/${game}`}>Enter</a>
+              </Button>
+            )
           ) : (
             <Button disabled className="select-none">
               Enter
